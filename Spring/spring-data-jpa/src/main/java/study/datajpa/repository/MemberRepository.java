7db546eee8e992bf0186289dbb92f072f3918f65
fbgjung <fbgjung@gmail.com>
2024-08-17 16:19:07 +0900

    Section4: 쿼리 메소드 기능 - @Query

--- Spring/spring-data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ Spring/spring-data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,9 +3,12 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
@@ -15,5 +18,25 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
 //    @Query(name="Member.findByUsername")
     List<Member> findByUsername(@Param("username") String username);
+
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    // 단순히 값 하나를 조회
+    @Query("select m.username from Member m")
+    List<String> findUsernameList();
+
+    // DTO로 직접 조회
+    @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
+
+    // Collection in
+    @Query("select m from Member m where m.username in :names")
+    List<Member> findByNames(@Param("names") Collection<String> names);
+
+    // 반환타입
+    List<Member> findListByUsername(String username); // 컬렉션
+    Member findMemberByUsername(String username); // 단건
+    Optional<Member> findOptionalByUsername(String username); // 단건 Optional
 
 }
--- Spring/spring-data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ Spring/spring-data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -5,9 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -17,6 +21,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 class MemberRepositoryTest {
 
     @Autowired MemberRepository memberRepository;
+    @Autowired TeamRepository teamRepository;
 
     @Test
     public void testMember() {
@@ -91,6 +96,94 @@ class MemberRepositoryTest {
         List<Member> result = memberRepository.findByUsername("AAA");
         Member findMember = result.get(0);
         assertThat(findMember).isEqualTo(m1);
+
+    }
+
+    @Test
+    public void testQuery() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findUser("AAA", 10);
+        Member findMember = result.get(0);
+        assertThat(findMember).isEqualTo(m1);
+
+    }
+
+    @Test
+    public void findUsernameList() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<String> usernameList = memberRepository.findUsernameList();
+        for (String s : usernameList) {
+            System.out.println(s);
+        }
+
+    }
+
+    @Test
+    public void findMemberDto() {
+        Team team = new Team("teamA");;
+        teamRepository.save(team);
+
+        Member m1 = new Member("AAA", 10);
+        m1.setTeam(team);
+        memberRepository.save(m1);
+
+        List<MemberDto> memberDto = memberRepository.findMemberDto();
+        for (MemberDto dto : memberDto) {
+            System.out.println(dto);
+
+        }
+
+    }
+
+    @Test
+    public void findByNames() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByNames(Arrays.asList("AAA", "BBB"));
+        for (Member member : result) {
+            System.out.println(member);
+            
+        }
+
+    }
+
+    @Test
+    public void returnType() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        Member findMember = memberRepository.findMemberByUsername("AAA");
+        System.out.println(findMember); // Member(id=1, username=AAA, age=10)
+
+        // 중요!
+        // Collection 조회시 없는 데이터를 조회해도 null 이 아니다.
+        // empty collection 반환해준다.
+        // null 이 아님이 보장이 되기 때문에 그냥 반환 받으면 된다.
+        List<Member> result = memberRepository.findListByUsername("abc");
+        System.out.println("result = " + result.size()); // result = 0
+
+        // 단건 조회는 null 반환
+        Member findNullMember = memberRepository.findMemberByUsername("abc");
+        System.out.println(findNullMember); // null (스프링 데이터는 try catch 로 감싸서 exception 발생시키지 않고 null로 반환을 해버린다.)
+
+        // Optional
+        // data 있을지 없을지 모르겠을 경우
+        // client 에 처리를 넘긴다.
+        Optional<Member> findOptionalMember = memberRepository.findOptionalByUsername("abc");
+        System.out.println(findOptionalMember); // Optional.empty
 
     }
 }
